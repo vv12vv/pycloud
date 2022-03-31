8171676b305e483e015033447c0acf8742066fb9
vv12vv <mmvv_mm@mail.ru>
2022-03-31 18:54:16 +0300

    with voice

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -6,3 +6,10 @@ else
     echo "$dir directory does not exist, exiting."
     exit 101
 fi
+echo "The heaviest subfolder of $dir is: "
+top1=$(du -S $dir | sort -rn | head -n 1 | awk '{print $2 "\n"}' )
+disk=$(du -h $top1 | awk '{print $1}')
+echo -e "$top1"
+echo -e "$disk"
+echo "The heaviest subfolder of $dir is: $top1" | festival --tts
+
